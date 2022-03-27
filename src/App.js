03e10bd38a16818130064afe71b68fc934d4bc96
diankhai <56186974+diankhai@users.noreply.github.com>
2022-03-27 21:45:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ export default function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Main /> */}
-      <Detail />
-      {/* <h1>Hello CodeSandbox</h1> */}
-      {/* <h2>Start editing to see some magic happen!</h2> */}
+      <div className="main">
+        <Detail />
+      </div>
     </div>
   );
 }
